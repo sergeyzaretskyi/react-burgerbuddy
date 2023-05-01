437dba78d75511c3ae91e333c2a6a020a4ea11e2
Sergey Zaretskyi <sergeyzaretskyi@proton.me>
2023-05-01 10:22:14 +0300

    feat: Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header/Header';
-import { Menu } from './components/Menu/Menu';
+import { Home } from './pages/Home';
+import { Cart } from './pages/Cart';
+import { User } from './pages/User';
+import { Service } from './pages/Service';
+import { NotFound } from './pages/NotFound';
 
 export const App = () => {
   return (
     <>
       <Header />
-      <Menu />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/service" element={<Service />} />
+      </Routes>
     </>
   );
 };
