985400861d4e1f7b2c524a99d5beef828805c2f8
Sergey Zaretskyi <sergeyzaretskyi@proton.me>
2023-05-02 11:33:22 +0300

    refactor: Remove unnecessary Fragment

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,46 +6,44 @@ export const Card = ({ name, title, bunOptions, label, price }) => {
   const [activeBunType, setActiveBunType] = useState(0);
 
   return (
-    <>
-      <div className={styles.card}>
-        <img
-          src={`/img/burgers/${name}.jpg`}
-          alt={title}
-          title={title}
-          width="340"
-          height="220"
-        />
+    <div className={styles.card}>
+      <img
+        src={`/img/burgers/${name}.jpg`}
+        alt={title}
+        title={title}
+        width="340"
+        height="220"
+      />
 
-        {label && <span className={styles.label}>{label.toUpperCase()}</span>}
+      {label && <span className={styles.label}>{label.toUpperCase()}</span>}
 
-        <div className={styles.cardContent}>
-          <Heading tag="h3" text={title} />
+      <div className={styles.cardContent}>
+        <Heading tag="h3" text={title} />
 
-          <p className={styles.bunType}>
-            {bunOptions.map((option, index) => {
-              const isActive = index === activeBunType;
+        <p className={styles.bunType}>
+          {bunOptions.map((option, index) => {
+            const isActive = index === activeBunType;
 
-              return (
-                <span
-                  key={`${name}-${option}`}
-                  onClick={() => setActiveBunType(index)}
-                  className={`${isActive ? styles.active : ''}`}
-                >
-                  {option}
-                </span>
-              );
-            })}
-          </p>
+            return (
+              <span
+                key={`${name}-${option}`}
+                onClick={() => setActiveBunType(index)}
+                className={`${isActive ? styles.active : ''}`}
+              >
+                {option}
+              </span>
+            );
+          })}
+        </p>
 
-          <div className={styles.cardBottom}>
-            <p className={styles.price}>${price}</p>
+        <div className={styles.cardBottom}>
+          <p className={styles.price}>${price}</p>
 
-            <button className={styles.cartButton}>
-              Add to cart <span className={styles.addedCount}>3</span>
-            </button>
-          </div>
+          <button className={styles.cartButton}>
+            Add to cart <span className={styles.addedCount}>3</span>
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
